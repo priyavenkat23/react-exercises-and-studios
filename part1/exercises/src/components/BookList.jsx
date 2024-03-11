f1a1b7112facd88a86cdcc754340d55f44c10022
priyavenkat23 <shanmugapriya.gunaseelan@gmail.com>
2024-03-11 17:24:01 -0500

    react part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "The Boxcar children";
+   let book1 = "https://m.media-amazon.com/images/I/717gwL0i2CL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71a7QtIhPVL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/61ZcCPDXBxL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Chapter 1" />
+         <img src={book2} alt="Chapter 2" />
+         <img src={book3} alt="Chapter 3" />
       </div>      
    );
 }
